469132caadd0ca6a68604e7cd6e7f0fe3658bcd4
LeHuynhDuc0307 <2001210765@hufi.edu.vn>
2024-06-08 23:57:48 +0700

    Fix voucher
    Chưa sửa giao diện

--- app/src/main/java/com/app/roomzy/Adapter/VoucherAdapter.java
+++ app/src/main/java/com/app/roomzy/Adapter/VoucherAdapter.java
@@ -17,48 +17,61 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
-    public class VoucherAdapter extends ArrayAdapter<Voucher> {
-        private Context context;
-        private int layoutItem;
-        private List<Voucher> voucherList;
+public class VoucherAdapter extends ArrayAdapter<Voucher> {
+    private Context context;
+    private int layoutItem;
+    private List<Voucher> voucherList;
 
-        public VoucherAdapter(Context context, int layoutItem, List<Voucher> voucherList) {
-            super(context, layoutItem, voucherList);
-            this.context = context;
-            this.layoutItem = layoutItem;
-            this.voucherList = voucherList;
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            if (convertView == null) {
-                convertView = LayoutInflater.from(context).inflate(layoutItem, parent, false);
-            }
-
-            Voucher voucher = voucherList.get(position);
-
-            ImageView imgVoucher = convertView.findViewById(R.id.imgVoucher);
-            TextView tvGiamGia = convertView.findViewById(R.id.tvGiamGia);
-            TextView tvName = convertView.findViewById(R.id.tvNameVoucher);
-            TextView tvMoTa = convertView.findViewById(R.id.tvMoTa);
-
-            if (voucher != null) {
-                tvGiamGia.setText(voucher.getGiamGia());
-                tvName.setText(voucher.getTenVoucher());
-                tvMoTa.setText(voucher.getMoTa());
-
-                Glide.with(context)
-                        .load(voucher.getImageURL())
-                        .into(imgVoucher);
-            }
-
-            return convertView;
-        }
-
-        @Override
-        public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            return getView(position, convertView, parent);
-        }
+    public VoucherAdapter(Context context, int layoutItem, List<Voucher> voucherList) {
+        super(context, layoutItem, voucherList);
+        this.context = context;
+        this.layoutItem = layoutItem;
+        this.voucherList = voucherList;
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(layoutItem, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.imgVoucher = convertView.findViewById(R.id.imgVoucher);
+            viewHolder.tvGiamGia = convertView.findViewById(R.id.tvGiamGia);
+            viewHolder.tvName = convertView.findViewById(R.id.tvNameVoucher);
+            viewHolder.tvMoTa = convertView.findViewById(R.id.tvMoTa);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Voucher voucher = voucherList.get(position);
+
+        if (voucher != null) {
+            viewHolder.tvGiamGia.setText(voucher.getGiamGia());
+            viewHolder.tvName.setText(voucher.getTenVC());
+            viewHolder.tvMoTa.setText(voucher.getMoTa());
+            Glide.with(context)
+                    .load(voucher.getHinh())
+                    .into(viewHolder.imgVoucher);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return getView(position, convertView, parent);
+    }
+
+    static class ViewHolder {
+        ImageView imgVoucher;
+        TextView tvGiamGia;
+        TextView tvName;
+        TextView tvMoTa;
+    }
+}
+
+
 
--- app/src/main/java/com/app/roomzy/Models/Voucher.java
+++ app/src/main/java/com/app/roomzy/Models/Voucher.java
@@ -1,78 +1,79 @@
 package com.app.roomzy.Models;
 
-public class Voucher {
-    private String tenVoucher;
-    private String moTa;
-    private String imageURL;
-    private String giamGia;
-    //==================
-    private String maVoucher;
-    private int thoiHan;
-    private double giaToiDa;
+        public class Voucher {
+        private String TenVC;
+        private String MoTa;
+        private String Hinh;
+        private String GiamGia;
+        private String MaVC;
+        private int ThoiHan;
+        private double GiaToiDa;
 
-    public Voucher() {
-    }
+        public Voucher(){
 
-    public Voucher(String tenVoucher, String moTa, String imageURL, String giamGia) {
-        this.tenVoucher = tenVoucher;
-        this.moTa = moTa;
-        this.imageURL = imageURL;
-        this.giamGia = giamGia;
-    }
+        }
+        public Voucher(String tenVC, String moTa, String giamGia, String hinh) {
+            this.TenVC = tenVC;
+            this.MoTa = moTa;
+            this.GiamGia = giamGia;
+            this.Hinh = hinh;
+        }
+        public String getTenVC() {
+            return TenVC;
+        }
 
-    public String getTenVoucher() {
-        return tenVoucher;
-    }
+        public void setTenVC(String tenVC) {
+            TenVC = tenVC;
+        }
 
-    public void setTenVoucher(String tenVoucher) {
-        this.tenVoucher = tenVoucher;
-    }
+        public String getMoTa() {
+            return MoTa;
+        }
 
-    public String getMoTa() {
-        return moTa;
-    }
+        public void setMoTa(String moTa) {
+            MoTa = moTa;
+        }
 
-    public void setMoTa(String moTa) {
-        this.moTa = moTa;
-    }
+        public String getHinh() {
+            return Hinh;
+        }
 
-    public String getImageURL() {
-        return imageURL;
-    }
+        public void setHinh(String hinh) {
+            Hinh = hinh;
+        }
 
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
+        public String getGiamGia() {
+            return GiamGia;
+        }
 
-    public String getGiamGia() {
-        return giamGia;
-    }
+        public void setGiamGia(String giamGia) {
+            GiamGia = giamGia;
+        }
 
-    public void setGiamGia(String giamGia) {
-        this.giamGia = giamGia;
-    }
+        public String getMaVC() {
+            return MaVC;
+        }
 
-    public String getMaVoucher() {
-        return maVoucher;
-    }
+        public void setMaVC(String maVC) {
+            MaVC = maVC;
+        }
 
-    public void setMaVoucher(String maVoucher) {
-        this.maVoucher = maVoucher;
-    }
+        public int getThoiHan() {
+            return ThoiHan;
+        }
 
-    public int getThoiHan() {
-        return thoiHan;
-    }
+        public void setThoiHan(int thoiHan) {
+            ThoiHan = thoiHan;
+        }
 
-    public void setThoiHan(int thoiHan) {
-        this.thoiHan = thoiHan;
-    }
+        public double getGiaToiDa() {
+            return GiaToiDa;
+        }
 
-    public double getGiaToiDa() {
-        return giaToiDa;
-    }
+        public void setGiaToiDa(double giaToiDa) {
+            GiaToiDa = giaToiDa;
+        }
 
-    public void setGiaToiDa(double giaToiDa) {
-        this.giaToiDa = giaToiDa;
-    }
 }
+
+
